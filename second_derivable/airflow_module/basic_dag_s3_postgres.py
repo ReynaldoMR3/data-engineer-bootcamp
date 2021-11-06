9f1da0e6364c8654612c1333d7d20aa4460ef48c
rmendezr <reynaldomendezr3@gmail.com>
2021-11-06 16:10:09 -0600

    adding changes to dag

--- second_derivable/airflow_module/basic_dag_s3_postgres.py
+++ second_derivable/airflow_module/basic_dag_s3_postgres.py
@@ -1,15 +1,32 @@
 from datetime import datetime
 from airflow import DAG
+import airflow.utils.dates
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from datetime import datetime, timedelta
 
-def print_hello():
-    return 'Hello world from first Airflow DAG!'
 
-dag = DAG('hello_world', description='Hello World DAG',
-          schedule_interval='0 12 * * *',
-          start_date=datetime(2021, 11, 6), catchup=False)
+default_args = {
+    'owner' : 'reynaldo.mendez',
+    'depends_on_past' : False,
+    'start_date' : airflow.utils.dates.days_ago(1),
+    'email' : ["reynaldomendezr3@gmail.com"],
+    'email_on_failure' : True,
+    'email_on_retry' : True,
+    'retries' : 1,
+    'retry_delay' : timedelta(minutes=5)
+} 
 
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
-
-hello_operator
+with DAG(
+    dag_id='postgres_create_extension',
+    default_args=default_args,
+    schedule_interval='@once'
+) as dag:
+    create_postgres_extension = PostgresOperator(
+        task_id="create_aws_s3_extension",
+        postgress_conn_id='postgres_default',
+        sql="""
+            CREATE EXTENSION aws_s3 CASCADE;
+          """
+    )
